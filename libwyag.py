5ade0f75b4fd4f5a2ec613b8896daf6f551a2445
Joe Vincent <hellojustjoe@gmail.com>
2025-02-05 13:04:42 -0600

    Setup wyag file, made executable, set up imports for libwyag.py

---
+++ libwyag.py
@@ -0,0 +1,11 @@
+import argparse
+import configparser
+from datetime import datetime
+import grp, pwd
+from fnmatch import fnmatch
+import hashlib
+from math import ceil
+import os
+import re
+import sys
+import zlib
